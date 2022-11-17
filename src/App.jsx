708cf8a47979e24b65fe7b852b252843fce6d93d
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-11-17 13:40:54 +0000

    update homepage url

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Router>
       <Navigation/>
         <Routes>
-          <Route exact path = "/react_movieDiscoverReviewApp/" element = {<Home/>} />
+          <Route exact path = "/" element = {<Home/>} />
           <Route exact path = "/Movies" element = {<Movies/>} />
           <Route exact path = "/About" element = {<About/>} />
           
