0a14e3c64ec3d89c8380191c8cc6273050ed19ef
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-11-17 13:37:17 +0000

    help

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,7 +17,7 @@ function About() {
     <>
     {/* <AboutPage>
           <h2>NETPICKS. Discover new movies. Review your favourites!</h2>
-        <h3>Work in Progress, full functionality; Coming Soon...</h3>
+        
     </AboutPage> */}
     <Newsletter/>  
     <Footer/>
